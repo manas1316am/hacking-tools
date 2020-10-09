bd8c50f53057ff135426c66a1184181351ca14ce
manas1316am <ad1316a@gmail.com>
2020-10-09 23:28:15 +0530

    getting current mac address

--- macChanger.py
+++ macChanger.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
@@ -27,7 +28,19 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig" + interface + " up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(
+        br'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("\n", "[-] Could not read MAC address.")
+
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC > " + str(current_mac))
+
 # change_mac(options.interface, options.new_mac)
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
