950062c9e74e9aa0502f9115e087f13ce7c2f9ad
manas1316am <ad1316a@gmail.com>
2020-10-10 10:14:42 +0530

    checking the IP address

--- network_scanner.py
+++ network_scanner.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-from scapy.all import *
+import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.all.arping(ip)
+    ''' Using ARP(Address Resolution Protocol) 
+    request to ask who is the specific IP'''
+    # pdst is the IP address module of scapy
+    arp_request = scapy.ARP(pdst=ip)
+    # Print the summary of the responce.
+    print(arp_request.summary())
 
 
 scan("192.168.0.1/24")
