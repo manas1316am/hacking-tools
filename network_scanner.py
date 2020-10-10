2039395acf4dbc72c9bf5eb31a0bd6340af799ff
manas1316am <ad1316a@gmail.com>
2020-10-10 10:52:32 +0530

    Creating the broadcast frame

--- network_scanner.py
+++ network_scanner.py
@@ -8,8 +8,19 @@ def scan(ip):
     request to ask who is the specific IP'''
     # pdst is the IP address module of scapy
     arp_request = scapy.ARP(pdst=ip)
-    # Print the summary of the responce.
-    print(arp_request.summary())
+    arp_request.show()
+
+    # Created an ethernet frame
+    ''' using Ether means that broadcast message 
+    should be sent to all devices'''
+    # dst is the DestMACField of the scapy.Ether()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+
+    # Concatenating the broadcast and arp_request
+    # use '/' in scapy to concatenate
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
 
 
 scan("192.168.0.1/24")
